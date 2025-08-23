ac370805441b2e056a21b9d1786f80ce050bac32
Nguyễn Thị Thanh Hiền <ngthithanhhien04@gmail.com>
2025-08-23 14:31:59 +0700

    feat: Bài 3

---
+++ Tuan01/src/Car.ts
@@ -0,0 +1,18 @@
+//3. Create a class Car with properties brand, model, year. Write a method to show car info.
+export class Car {
+  brand: string;
+  model: string;
+  year: number;
+
+  constructor(brand: string, model: string, year: number) {
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+  }
+
+  showInfo(): void {
+    console.log(
+      `Brand: ${this.brand}, Model: ${this.model}, Year: ${this.year}`
+    );
+  }
+}
