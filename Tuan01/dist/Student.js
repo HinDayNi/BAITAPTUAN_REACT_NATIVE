9863d27efd78e5404f59d8952eebd12cd41aa175
Nguyễn Thị Thanh Hiền <ngthithanhhien04@gmail.com>
2025-08-23 14:16:51 +0700

    feat: Bài 2

---
+++ Tuan01/dist/Student.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Student = void 0;
+//2. Write a class Student extending Person with an additional attribute grade. Add a method to
+//display all info.
+const Person_1 = require("./Person");
+class Student extends Person_1.Person {
+    constructor(name, age, grade) {
+        super(name, age);
+        this.grade = grade;
+    }
+    displayInfo() {
+        super.displayInfo();
+        console.log(`Grade: ${this.grade}`);
+    }
+}
+exports.Student = Student;
---
+++ Tuan01/src/Student.ts
@@ -0,0 +1,16 @@
+//2. Write a class Student extending Person with an additional attribute grade. Add a method to
+//display all info.
+import { Person } from "./Person";
+
+export class Student extends Person {
+  grade: number;
+
+  constructor(name: string, age: number, grade: number) {
+    super(name, age);
+    this.grade = grade;
+  }
+  displayInfo(): void {
+    super.displayInfo();
+    console.log(`Grade: ${this.grade}`);
+  }
+}
