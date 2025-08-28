7d58c2050626e44b4b6c3df4b89c28fdbecb7fee
Nguyễn Thị Thanh Hiền <ngthithanhhien04@gmail.com>
2025-08-28 16:07:47 +0700

    feat: Bài 25

--- Tuan01/dist/Shape.js
+++ Tuan01/dist/Shape.js
@@ -3,5 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Shape = void 0;
 //13. Create an abstract class Shape with method area(). Implement Square and Circle.
 class Shape {
+    static describe() {
+        console.log("Shapes are geometric figures like circles, squares, and triangles.");
+    }
 }
 exports.Shape = Shape;
--- Tuan01/src/Shape.ts
+++ Tuan01/src/Shape.ts
@@ -1,4 +1,9 @@
 //13. Create an abstract class Shape with method area(). Implement Square and Circle.
 export abstract class Shape {
   abstract area(): number;
+  static describe(): void {
+    console.log(
+      "Shapes are geometric figures like circles, squares, and triangles."
+    );
+  }
 }
