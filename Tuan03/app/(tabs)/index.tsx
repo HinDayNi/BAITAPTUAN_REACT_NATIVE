fd3e3635afdf799ee8f4deaef3371e6a03ec47ed
Nguyễn Thị Thanh Hiền <ngthithanhhien04@gmail.com>
2025-09-06 13:44:40 +0700

    feat: Bài 1_a

--- Tuan03/app/(tabs)/index.tsx
+++ Tuan03/app/(tabs)/index.tsx
@@ -1,72 +1,72 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+// import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function FirstScreen() {
-  return (
-    <View style={styles.container}>
-      <View style={{ flex: 1 }}>
-        <View style={styles.logo} />
-      </View>
-      <View style={{ flex: 1 }}>
-        <Text style={styles.title}>GROW {"\n"} YOUR BUSINESS</Text>
-        <Text style={styles.subtitle}>
-          We will help you to grow your business using{"\n"}online server
-        </Text>
-      </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>LOGIN</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.text}>SIGN UP</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-}
+// export default function FirstScreen() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={{ flex: 1 }}>
+//         <View style={styles.logo} />
+//       </View>
+//       <View style={{ flex: 1 }}>
+//         <Text style={styles.title}>GROW {"\n"} YOUR BUSINESS</Text>
+//         <Text style={styles.subtitle}>
+//           We will help you to grow your business using{"\n"}online server
+//         </Text>
+//       </View>
+//       <View style={styles.buttonContainer}>
+//         <TouchableOpacity style={styles.button}>
+//           <Text style={styles.text}>LOGIN</Text>
+//         </TouchableOpacity>
+//         <TouchableOpacity style={styles.button}>
+//           <Text style={styles.text}>SIGN UP</Text>
+//         </TouchableOpacity>
+//       </View>
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#00CCF9",
-  },
-  logo: {
-    width: 150,
-    height: 150,
-    borderRadius: 80,
-    borderWidth: 15,
-    borderColor: "black",
-    borderStyle: "solid",
-    marginTop: 150,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-    paddingTop: 80,
-  },
-  subtitle: {
-    fontSize: 16,
-    fontWeight: "bold",
-    textAlign: "center",
-    paddingTop: 50,
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: "row",
-    gap: 20,
-  },
-  text: {
-    textAlign: "center",
-    lineHeight: 55,
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  button: {
-    width: 100,
-    height: 50,
-    borderRadius: 10,
-    backgroundColor: "#E3C000",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "#00CCF9",
+//   },
+//   logo: {
+//     width: 150,
+//     height: 150,
+//     borderRadius: 80,
+//     borderWidth: 15,
+//     borderColor: "black",
+//     borderStyle: "solid",
+//     marginTop: 150,
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: "bold",
+//     textAlign: "center",
+//     paddingTop: 80,
+//   },
+//   subtitle: {
+//     fontSize: 16,
+//     fontWeight: "bold",
+//     textAlign: "center",
+//     paddingTop: 50,
+//   },
+//   buttonContainer: {
+//     flex: 1,
+//     flexDirection: "row",
+//     gap: 20,
+//   },
+//   text: {
+//     textAlign: "center",
+//     lineHeight: 55,
+//     fontSize: 20,
+//     fontWeight: "bold",
+//   },
+//   button: {
+//     width: 100,
+//     height: 50,
+//     borderRadius: 10,
+//     backgroundColor: "#E3C000",
+//   },
+// });
